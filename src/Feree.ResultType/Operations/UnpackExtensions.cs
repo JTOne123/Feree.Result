938f3e21cfd35009723df88443b2eb79d27ed48f
Bartosz <bartosz.k.gdz@gmail.com>
2019-06-03 13:26:42 +0200

    unpack operators

--- src/Feree.ResultType/Operations/UnpackExtensions.cs
+++ src/Feree.ResultType/Operations/UnpackExtensions.cs
@@ -6,21 +6,29 @@ namespace Feree.ResultType.Operations
 {
     public static class UnpackExtensions
     {
-        public static void Unpack<T>(this IResult<T> result, out T payload, out IError error)
+        public static bool IsSuccess<T>(this IResult<T> result, out T payload, out IError error)
         {
             switch (result)
             {
                 case Success<T> success:
                     payload = success.Payload;
-                    error = null;
-                    return;
+                    error = default;
+                    return true;
                 case Failure<T> failure:
                     payload = default;
                     error = failure.Error;
-                    return;
+                    return false;
                 default:
                     throw new InvalidOperationException("unknown result type");
             }
+        }
+
+        public static bool IsFailure<T>(this IResult<T> result, out T payload, out IError error) => !result.IsSuccess(out payload, out error);
+
+        public static (T payload, IError error) Unpack<T>(this IResult<T> result)
+        {
+            result.IsSuccess(out var payload, out var error);
+            return (payload, error);
         }
     }
 }
